434dea522f7747ca32063f479e903cb31190a514
Bogdan Dobrica <bdobrica@gmail.com>
2022-02-03 20:34:08 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker ps | grep yolov4 | awk '{print $1}' | xargs docker stop
-docker ps | grep modnet | awk '{print $1}' | xargs docker stop
-docker ps | grep u2net | awk '{print $1}' | xargs docker stop
+docker ps | grep yolov4 | awk '{print $1}' | xargs docker stop > /dev/null 2>&1
+docker ps | grep modnet | awk '{print $1}' | xargs docker stop > /dev/null 2>&1
+docker ps | grep u2net | awk '{print $1}' | xargs docker stop > /dev/null 2>&1
