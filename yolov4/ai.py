e49c8091a6aba24dbbfc8d82fcd4aeaeb141a3e9
Bogdan Dobrica <bdobrica@gmail.com>
2022-02-03 20:27:35 +0200

    Update ai.py

--- yolov4/ai.py
+++ yolov4/ai.py
@@ -15,7 +15,7 @@ import cv2
 from yolov4 import Darknet, non_max_suppression, scale_coords
 
 class AIDaemon(Daemon):
-    def ai(self, source_file, prepared_file):
+    def ai(self, source_file, prepared_file, **metadata):
         pid = os.fork()
         if pid != 0:
             return
@@ -127,13 +127,12 @@ class AIDaemon(Daemon):
                             color = (255, 255, 255),
                             thickness = 1,
                             lineType = cv2.LINE_AA)
-
-            results.sort(key = lambda x : x.get('area') or 0.0, reverse = True)
-
-            with prepared_file.open('w') as f:
-                json.dump({'results':results}, f)
+            cv2.imwrite(str(prepared_file), img_copy)
             
-            cv2.imwrite(str(prepared_file.parent / (prepared_file.stem + '.png')), img_copy)
+            results.sort(key = lambda x : x.get('area') or 0.0, reverse = True)
+            json_file = prepared_file.with_suffix('.json')
+            with json_file.open('w') as f:
+                json.dump({'results':results}, f)
         except Exception as e:
             pass
         
@@ -147,17 +146,37 @@ class AIDaemon(Daemon):
         MAX_FORK = int(os.environ.get("MAX_FORK", 8))
         CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 4096))
 
-        staged_files = sorted([ f for f in Path(STAGED_PATH).glob("*") if f.is_file() ], key = lambda f : f.stat().st_mtime)
+        staged_files = sorted([ f for f in Path(STAGED_PATH).glob("*")\
+            if f.is_file() and f.suffix != '.json'],
+            key = lambda f : f.stat().st_mtime)
         source_files = [ f for f in Path(SOURCE_PATH).glob("*") if f.is_file() ]
         source_files_count = len(source_files)
 
         while source_files_count < MAX_FORK and staged_files:
             source_files_count += 1
             staged_file = staged_files.pop(0)
-            source_file = Path(SOURCE_PATH) / staged_file.name
-            prepared_file = Path(PREPARED_PATH) / (staged_file.stem + '.json')
 
-            with staged_file.open('rb') as src_fp, source_file.open('wb') as dst_fp:
+            meta_file = staged_file.with_suffix('.json')
+            if meta_file.is_file():
+                with meta_file.open('r') as fp:
+                    try:
+                        image_metadata = json.load(fp)
+                    except:
+                        image_metadata = {}
+            image_metadata = {
+                **{
+                    'extension': staged_file.suffix,
+                    'background': '',
+                },
+                **image_metadata
+            }
+
+            source_file = Path(SOURCE_PATH) / staged_file.name
+            prepared_file = Path(PREPARED_PATH) /\
+                (staged_file.stem + image_metadata['extension'])
+
+            with staged_file.open('rb') as src_fp,\
+                 source_file.open('wb') as dst_fp:
                 while True:
                     chunk = src_fp.read(CHUNK_SIZE)
                     if not chunk:
@@ -165,7 +184,7 @@ class AIDaemon(Daemon):
                     dst_fp.write(chunk)
 
             staged_file.unlink()
-            self.ai(source_file, prepared_file)
+            self.ai(source_file, prepared_file, **image_metadata)
     
     def run(self):
         signal.signal(signal.SIGCHLD, signal.SIG_IGN)
