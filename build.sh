e647515411145df4cc592cb7b97841b2a2106b26
Bogdan Dobrica <bdobrica@gmail.com>
2022-02-03 20:34:00 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ if [ ! -f "u2net/u2net.pth" ]; then
     # Credits: https://github.com/xuebinqin/U-2-Net
     gdown https://drive.google.com/uc?id=1ao1ovG1Qtx4b7EoskHXmi2E9rp5CHLcZ --output u2net/u2net.pth
 fi
-if [ ! -f "yolo4/yolov4.weights" ]; then
+if [ ! -f "yolov4/yolov4.weights" ]; then
     # Credits: https://github.com/kiyoshiiriemon/yolov4_darknet
-    gdown https://drive.google.com/uc?id=1cewMfusmPjYWbrnuJRuKhPMwRe_b9PaT --output yolo4/yolov4.weights
+    gdown https://drive.google.com/uc?id=1cewMfusmPjYWbrnuJRuKhPMwRe_b9PaT --output yolov4/yolov4.weights
 fi
 docker build -f modnet/Dockerfile -t modnet .
 docker build -f u2net/Dockerfile -t u2net .
