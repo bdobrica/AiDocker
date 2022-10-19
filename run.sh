4ad6bdc1de0ff12164b42de63ba3c23742a46a74
Bogdan Dobrica <bdobrica@gmail.com>
2022-10-19 21:32:20 +0300

    Update run and stop scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --env-file ./docker.env -d -p 127.0.0.1:5000:5000/tcp yolov4
-docker run --env-file ./docker.env -d -p 127.0.0.1:5001:5000/tcp modnet
-docker run --env-file ./docker.env -d -p 127.0.0.1:5002:5000/tcp u2net
-docker run --env-file ./docker.env -d -p 127.0.0.1:5003:5000/tcp nudenet
+docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5000:5000/tcp yolov4
+docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5001:5000/tcp modnet
+docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5002:5000/tcp u2net
+docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5003:5000/tcp nudenet
