ab98ec1e76ef82c521cf5e1c7aecab0851b16a19
Bogdan Dobrica <bdobrica@gmail.com>
2022-10-19 21:17:08 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 docker run --env-file ./docker.env -d -p 127.0.0.1:5000:5000/tcp yolov4
 docker run --env-file ./docker.env -d -p 127.0.0.1:5001:5000/tcp modnet
 docker run --env-file ./docker.env -d -p 127.0.0.1:5002:5000/tcp u2net
+docker run --env-file ./docker.env -d -p 127.0.0.1:5003:5000/tcp nudenet
