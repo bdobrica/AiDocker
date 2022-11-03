f943ff435187dc973c5cdbb9692c2ecee5465cc0
Bogdan Dobrica <bdobrica@gmail.com>
2022-11-03 22:06:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5001:5000/tcp modnet
 docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5002:5000/tcp u2net
 docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5003:5000/tcp nudenet
 docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5004:5000/tcp agenet
+docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5005:5000/tcp gfm34b2tt
+docker run --rm --env-file ./docker.env -d -p 127.0.0.1:5006:5000/tcp isnet
