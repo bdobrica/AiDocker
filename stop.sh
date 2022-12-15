1e25a6113a10bbb0d0d7ef70138deb8589cae042
Bogdan Dobrica <bdobrica@gmail.com>
2022-12-15 20:32:25 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 find . -type f -name "Dockerfile" | while read dockerfile; do
-  model_dir=$(dirname "{$dockerfile}")
+  model_dir=$(dirname "${dockerfile}")
   model_name=$(basename "${model_dir}")
   docker ps | grep "${model_name}" | awk '{print $1}' | xargs docker stop > /dev/null 2>&1
 done
