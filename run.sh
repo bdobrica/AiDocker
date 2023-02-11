f3bf2be064756b6c3f25d09e483de9f0326965ab
Bogdan Dobrica <bdobrica@gmail.com>
2023-02-11 22:04:39 +0200

    Updates for Docker

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ function detect_cuda {
             echo "No free GPU memory found, using CPU"
             return
         fi
+        echo "Free GPU memory found, using GPU"
         docker_args+=("--gpus" "all")
     else
         echo "nvidia-smi not found, using CPU"
