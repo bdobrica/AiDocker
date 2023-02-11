b547ab07e0ef6a7cc1eff5cf23e4d095497259ed
Bogdan Dobrica <bdobrica@gmail.com>
2023-02-11 20:32:54 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-find . -name "Dockerfile" | while read dockerfile; do
-    model_dir=$(dirname ${dockerfile})
-    model_name=$(basename ${model_dir})
-    weights_file="${model_dir}/weights.txt"
+function build_container {
+    local dockerfile="$1"
+    local model_dir=$(dirname ${dockerfile})
+    local model_name=$(basename ${model_dir})
+    local weights_file="${model_dir}/weights.txt"
 
     echo "Building ${model_name} ..."
 
@@ -11,7 +12,7 @@ find . -name "Dockerfile" | while read dockerfile; do
         cat "${weights_file}" | while read line; do
             line=$(echo ${line} | tr -d "\r" | tr -d "\n")
             if [ ! -z "${line}" ]; then
-                weight_file=${model_dir}/${line#*/}
+                local weight_file=${model_dir}/${line#*/}
                 if [ ! -f "${weight_file}" ]; then
                     wget "https://ublo.ro/wp-content/mirror/${line}" -O ${weight_file}
                 fi
@@ -19,4 +20,40 @@ find . -name "Dockerfile" | while read dockerfile; do
         done
     fi
     docker build -f "${dockerfile}" -t "${model_name}" .
-done
+}
+
+
+function parse_arguments {
+    while getopts ":hc:a" opt; do
+        case "${op}t" in
+            h)
+                echo "Usage: $0 [-h] [-c <container>] [-a]"
+                echo "  -h  Show this help message"
+                echo "  -c  Build only the specified container"
+                echo "  -a  Build all containers"
+                exit 0
+                ;;
+            c)
+                if [ -f "${OPTARG}/Dockerfile" ]; then
+                    build_container "${OPTARG}/Dockerfile"
+                    exit 0
+                fi
+                build_container "${OPTARG}"
+                exit 0
+                ;;
+            a)
+                echo "Building all containers ..."
+                find . -name "Dockerfile" | while read dockerfile; do
+                    build_container "${dockerfile}"
+                done
+                done
+                ;;
+            *)
+                echo "Invalid option: -${OPTARG}"
+                exit 1
+                ;;
+        esac
+    done
+}
+
+parse_arguments "$@"
