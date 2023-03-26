e05b0d426adcf013d7211ab87ae7a3615e381173
Bogdan Dobrica <bdobrica@gmail.com>
2023-03-26 21:25:34 +0300

    Convert VitGpt2 to Batch Processing
    - this will allow better usage of resources by grouping input files to form batch inputs for the model

--- vitgpt2/ai.py
+++ vitgpt2/ai.py
@@ -2,9 +2,6 @@
 import json
 import os
 import random
-import signal
-import sys
-import time
 import traceback
 from pathlib import Path
 
@@ -18,163 +15,111 @@ from transformers import (
     ViTFeatureExtractor,
 )
 
-from daemon import Daemon
+from daemon import AiBatch as Batch
+from daemon import AiBatchDaemon as Daemon
 
 __version__ = "0.8.12"
 
 
+class AiBatch(Batch):
+    def prepare(self) -> np.ndarray:
+        # Load the images
+        images = [
+            cv2.cvtColor(cv2.imread(str(source_file)), cv2.COLOR_BGR2RGB)
+            for source_file in self.source_files
+        ]
+
+        # Compute maximum image size
+        self.shapes = [im.shape for im in images]
+        self.im_h = max([shape[0] for shape in self.shapes])
+        self.im_w = max([shape[1] for shape in self.shapes])
+
+        # Augment images
+        images = [
+            cv2.resize(
+                im, (self.im_w, self.im_h), interpolation=cv2.INTER_LINEAR
+            )
+            for im in images
+        ]
+
+        # Parameters
+        self.max_length = max(
+            [int(metadata.get("max_length", 16)) for metadata in self.metadata]
+        )
+        self.num_beams = max(
+            [int(metadata.get("num_beams", 4)) for metadata in self.metadata]
+        )
+
+        # Build batch input
+        return np.stack(images)
+
+    def serve(self, inference_data: np.ndarray) -> None:
+        # Save the results
+        for file_idx, prediction in enumerate(inference_data):
+            result = {"caption": prediction.strip()}
+            with open(self.prepared_files[file_idx], "w") as fp:
+                json.dump(result, fp)
+
+
 class AIDaemon(Daemon):
     def load(self) -> None:
-        MODEL_DEVICE = os.environ.get("MODEL_DEVICE", "cuda:0")
+        MODEL_DEVICE = os.getenv("MODEL_DEVICE", "cuda:0")
         if MODEL_DEVICE.startswith("cuda") and not torch.cuda.is_available():
             MODEL_DEVICE = "cpu"
         self.device = torch.device(MODEL_DEVICE)
 
-        MODEL_PATH = os.environ.get("MODEL_PATH", "/opt/app/vitgpt2")
+        MODEL_PATH = os.getenv("MODEL_PATH", "/opt/app/vitgpt2")
 
         # The trick is to not initialize the model outside the forked process
         # It saves some time, but not much. At least no disk I/O
-        self.model_config = VisionEncoderDecoderConfig.from_pretrained(
+        model_config = VisionEncoderDecoderConfig.from_pretrained(
             MODEL_PATH, local_files_only=True
         )
-        self.model_sd = torch.load(
-            os.path.join(MODEL_PATH, "pytorch_model.bin"),
+        model_sd = torch.load(
+            (Path(MODEL_PATH) / "pytorch_model.bin").as_posix(),
             map_location=self.device,
         )
-        self.feature_extractor = ViTFeatureExtractor.from_pretrained(
+        feature_extractor = ViTFeatureExtractor.from_pretrained(
             MODEL_PATH, local_files_only=True
         )
-        self.tokenizer = AutoTokenizer.from_pretrained(
+        tokenizer = AutoTokenizer.from_pretrained(
             MODEL_PATH, local_files_only=True
         )
+        self.model = VisionEncoderDecoderModel(config=model_config)
+        self.model.to(self.device)
+        self.model.load_state_dict(model_sd)
 
-    def load_metadata(self, meta_file: Path) -> dict:
-        if not meta_file.is_file():
-            return {}
-        with open(meta_file, "r") as fp:
-            return json.load(fp)
-
-    def update_metadata(self, meta_file: Path, data: dict) -> None:
-        metadata = self.load_metadata(meta_file)
-        if "update_time" not in metadata:
-            metadata["update_time"] = time.time()
-        metadata.update(data)
-        with open(meta_file, "w") as fp:
-            json.dump(metadata, fp)
-
-    def ai(
-        self, source_file: Path, prepared_file: Path, meta_file: Path
-    ) -> None:
-        pid = os.fork()
-        if pid != 0:
-            return
-
+    def ai(self, batch: AiBatch) -> None:
         try:
             # Try reproducing the results
             torch.manual_seed(42)
             np.random.seed(42)
             random.seed(42)
 
-            # Initialize & Load Model
-            model = VisionEncoderDecoderModel(config=self.model_config)
-            model.to(self.device)
-            model.load_state_dict(self.model_sd)
-
-            # Load metadata
-            metadata = self.load_metadata(meta_file)
-
-            # Parameters
-            max_length = int(metadata.get("max_length", 16))
-            num_beams = int(metadata.get("num_beams", 4))
-
-            # Load image
-            im_orig = cv2.imread(str(source_file))
-            im_orig = cv2.cvtColor(im_orig, cv2.COLOR_BGR2RGB)
-
             # Inference
-            gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+            model_input = batch.prepare()
+            gen_kwargs = {
+                "max_length": batch.max_length,
+                "num_beams": batch.num_beams,
+            }
             pixel_values = self.feature_extractor(
-                images=[im_orig], return_tensors="pt"
+                images=model_input, return_tensors="pt"
             ).pixel_values
-            pixel_values = pixel_values.to(self.device)
-            output_ids = model.generate(pixel_values, **gen_kwargs)
-            preds = self.tokenizer.batch_decode(
+            output_ids = self.model.generate(pixel_values, **gen_kwargs)
+            predictions = self.tokenizer.batch_decode(
                 output_ids, skip_special_tokens=True
             )
-            results = [{"caption": pred.strip()} for pred in preds]
+            batch.serve(predictions)
+            batch.update_metadata({"processed": "true"})
 
-            json_file = prepared_file.with_suffix(".json")
-            with json_file.open("w") as f:
-                json.dump({"results": results}, f)
-
-            self.update_metadata(
-                meta_file,
-                {
-                    "processed": "true",
-                },
-            )
         except Exception as e:
+            batch.update_metadata({"processed": "error"})
             if os.environ.get("DEBUG", "false").lower() in ("true", "1", "on"):
                 print(traceback.format_exc())
-            self.update_metadata(
-                meta_file,
-                {
-                    "processed": "error",
-                },
-            )
-
-        source_file.unlink()
-        sys.exit()
-
-    def queue(self) -> None:
-        STAGED_PATH = os.environ.get("STAGED_PATH", "/tmp/ai/staged")
-        SOURCE_PATH = os.environ.get("SOURCE_PATH", "/tmp/ai/source")
-        PREPARED_PATH = os.environ.get("PREPARED_PATH", "/tmp/ai/prepared")
-        MAX_FORK = int(os.environ.get("MAX_FORK", 8))
-        CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 4096))
-
-        staged_files = sorted(
-            [
-                f
-                for f in Path(STAGED_PATH).glob("*")
-                if f.is_file() and f.suffix != ".json"
-            ],
-            key=lambda f: f.stat().st_mtime,
-        )
-        source_files = [f for f in Path(SOURCE_PATH).glob("*") if f.is_file()]
-        source_files_count = len(source_files)
-
-        while source_files_count < MAX_FORK and staged_files:
-            source_files_count += 1
-            staged_file = staged_files.pop(0)
-
-            meta_file = staged_file.with_suffix(".json")
-            source_file = Path(SOURCE_PATH) / staged_file.name
-            prepared_file = Path(PREPARED_PATH) / (
-                staged_file.stem + staged_file.suffix
-            )
-
-            with staged_file.open("rb") as src_fp, source_file.open(
-                "wb"
-            ) as dst_fp:
-                while True:
-                    chunk = src_fp.read(CHUNK_SIZE)
-                    if not chunk:
-                        break
-                    dst_fp.write(chunk)
-
-            staged_file.unlink()
-            self.ai(source_file, prepared_file, meta_file)
-
-    def run(self) -> None:
-        signal.signal(signal.SIGCHLD, signal.SIG_IGN)
-        while True:
-            self.queue()
-            time.sleep(float(os.environ.get("QUEUE_LATENCY", 1.0)))
 
 
 if __name__ == "__main__":
-    CHROOT_PATH = os.environ.get("CHROOT_PATH", "/opt/app")
-    PIDFILE_PATH = os.environ.get("PIDFILE_PATH", "/opt/app/run/ai.pid")
+    CHROOT_PATH = os.getenv("CHROOT_PATH", "/opt/app")
+    PIDFILE_PATH = os.getenv("PIDFILE_PATH", "/opt/app/run/ai.pid")
 
     AIDaemon(pidfile=PIDFILE_PATH, chroot=CHROOT_PATH).start()
