1b0a97eea555dcdda3725ca41d5b0cd625758dfa
Bogdan Dobrica <bdobrica@gmail.com>
2023-07-02 14:23:47 +0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -16,5 +16,6 @@ docker=$(detect_docker_command)
 find . -type f -name "Dockerfile" | while read dockerfile; do
   model_dir=$(dirname "${dockerfile}")
   model_name=$(basename "${model_dir}")
+  echo "Stopping ${model_name} ..."
   $docker ps | grep "${model_name}" | awk '{print $1}' | xargs $docker stop > /dev/null 2>&1
 done
