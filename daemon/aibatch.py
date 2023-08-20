5b537aa66dd0b005e00bc246d2277f610dd9718e
Bogdan Dobrica <bdobrica@gmail.com>
2023-08-20 15:04:15 +0300

    Update aibatch.py

--- daemon/aibatch.py
+++ daemon/aibatch.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Any, Iterable, Optional, Union
+from typing import Any, Iterable, List, Optional, Union
 
 from .daemon import PathLike
 from .filequeuemixin import FileQueueMixin
@@ -10,22 +10,20 @@ class AiBatch(FileQueueMixin):
     DEFAULT_EXTENSION = os.getenv("DEFAULT_EXTENSION", "png").lower()
 
     def _move_staged_files(self) -> None:
-        _ = [staged_file.rename(source_file) for staged_file, source_file in zip(self.staged_files, self.source_files)]
+        _ = [staged_file.rename(self.source_path / staged_file.name) for staged_file in self.staged_files]
+        self.source_files = [
+            self.source_path / staged_file.name
+            for staged_file in self.staged_files
+            if (self.source_path / staged_file.name).is_file()
+        ]
 
     def __init__(self, staged_files: Union[PathLike, Iterable[PathLike]]):
         if isinstance(staged_files, (str, Path)):
             staged_files = [staged_files]
-        self.staged_files = [
-            Path(staged_file) if isinstance(staged_file, str) else staged_file for staged_file in staged_files
-        ]
+        self.staged_files = [Path(staged_file) for staged_file in staged_files if Path(staged_file).is_file()]
         self.meta_files = [staged_file.with_suffix(".json") for staged_file in self.staged_files]
-        self.source_files = [self.SOURCE_PATH / staged_file.name for staged_file in self.staged_files]
         self.metadata = [AiBatch._load_metadata(meta_file) for meta_file in self.meta_files]
-        self.prepared_files = [
-            self.PREPARED_PATH
-            / (staged_file.stem + "." + self.metadata[index].get("output_extension", self.DEFAULT_EXTENSION))
-            for index, staged_file in enumerate(self.staged_files)
-        ]
+        self.source_files = []
         self._move_staged_files()
 
     def prepare(self) -> Any:
