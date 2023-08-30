35209e32599f9807c41016ea555a7548a633cf07
Bogdan Dobrica <bdobrica@gmail.com>
2023-08-30 20:28:37 +0300

    Update __main__.py

--- mpnetv2/ai/__main__.py
+++ mpnetv2/ai/__main__.py
@@ -12,12 +12,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     CHROOT_PATH = os.getenv("CHROOT_PATH", "/opt/app")
+    PIDFILE_PATH = os.getenv("PIDFILE_PATH", f"/opt/app/run/{args.daemon.lower()}.pid")
 
     if args.daemon.lower() in ("aibuilddaemon", "builddaemon", "build"):
-        PIDFILE_PATH = os.getenv("PIDFILE_PATH", "/opt/app/run/ai-build.pid")
         AiBuildDaemon(input_type=AiBuildBatch, pidfile=PIDFILE_PATH, chroot=CHROOT_PATH).start()
     elif args.daemon.lower() in ("aiquerydaemon", "querydaemon", "query"):
-        PIDFILE_PATH = os.getenv("PIDFILE_PATH", "/opt/app/run/query-ai.pid")
         AiQueryDaemon(input_type=AiQueryInput, pidfile=PIDFILE_PATH, chroot=CHROOT_PATH).start()
     else:
         raise ValueError(f"Unknown daemon type: {args.daemon}")
