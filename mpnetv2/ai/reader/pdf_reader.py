bea80bcd4840b343702e6103ba3e8a29b8e5e0a2
Bogdan Dobrica <bdobrica@gmail.com>
2023-09-10 17:25:46 +0300

    Update pdf_reader.py

--- mpnetv2/ai/reader/pdf_reader.py
+++ mpnetv2/ai/reader/pdf_reader.py
@@ -1,20 +1,20 @@
 from pathlib import Path
 from typing import List, Optional
 
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 
 from .text_item import TextItem
 
 
 def pdf_reader(path: Path, search_space: Optional[str] = None) -> List[TextItem]:
-    reader = PdfFileReader(path)
+    reader = PdfReader(path)
     return [
         TextItem(
-            text=reader.getPage(page_index).extractText(),
+            text=page.extract_text(),
             search_space=search_space or "",
             page=page_index,
             paragraph=None,
             path=path,
         )
-        for page_index in range(reader.getNumPages())
+        for page_index, page in enumerate(reader.pages)
     ]
