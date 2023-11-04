445c2665009c6fbe5326b21216a857d6ec3733ba
Bogdan Dobrica <bdobrica@gmail.com>
2023-11-04 21:03:53 +0200

    Refactor ZMQ Connection

--- api/callbacks/zero_queue/helpers.py
+++ api/callbacks/zero_queue/helpers.py
@@ -1,14 +1,36 @@
+import atexit
+import sys
+from functools import cached_property
+
 import zmq
 from flask import current_app
 
 
-def get_socket() -> zmq.Socket:
-    current_app.logger.debug("Creating ZeroMQ socket")
-    context = zmq.Context()
-    worker_timeout = current_app.config["zmq_worker_timeout"]
-    client_address = current_app.config["zmq_client_address"]
-    current_app.logger.debug("Connecting to ZeroMQ socket at %s", client_address)
-    socket: zmq.Socket = context.socket(zmq.REQ)
-    socket.setsockopt(zmq.RCVTIMEO, worker_timeout)
-    socket.connect(client_address)
-    return socket
+class Socket:
+    @cached_property
+    def _context(self) -> zmq.Context:
+        main_module = sys.modules["__main__"]
+        if not hasattr(main_module, "_context"):
+            main_module._context = zmq.Context()
+
+            def cleanup() -> None:
+                main_module = sys.modules["__main__"]
+                if hasattr(main_module, "_context"):
+                    main_module._context.term()
+
+            atexit.register(cleanup)
+        return main_module._context
+
+    def __enter__(self) -> zmq.Socket:
+        worker_timeout = current_app.config["zmq_worker_timeout"]
+        client_address = current_app.config["zmq_client_address"]
+
+        self._socket: zmq.Socket = self._context.socket(zmq.REQ)
+        self._socket.setsockopt(zmq.RCVTIMEO, worker_timeout)
+        self._socket.setsockopt(zmq.SNDTIMEO, worker_timeout)
+        self._socket.setsockopt(zmq.LINGER, 0)
+        self._socket.connect(client_address)
+        return self._socket
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self._socket.close()
--- api/callbacks/zero_queue/json.py
+++ api/callbacks/zero_queue/json.py
@@ -5,25 +5,24 @@ import zmq
 from flask import Response, current_app, request
 
 from .. import __version__
-from .helpers import get_socket
+from .helpers import Socket
 
 
 def get_json() -> Response:
-    current_app.logger.debug("Received request: %s", request.json)
-    socket = get_socket()
-    current_app.logger.debug("Sending request: %s", request.json)
-    socket.send_json(request.json)
+    with Socket() as socket:
+        current_app.logger.debug("Sending request: %s", request.json)
+        socket.send_json(request.json)
 
-    start_time = time.perf_counter()
-    try:
-        response = socket.recv_json()
-        current_app.logger.debug("Received response: %s", response)
-    except zmq.error.Again:
-        response = {"error": "timeout"}
-        socket.close()
-        current_app.logger.debug("Worker connection timeout")
+        start_time = time.perf_counter()
+        try:
+            response = socket.recv_json()
+            current_app.logger.debug("Received response: %s", response)
+        except zmq.error.Again:
+            response = {"error": "timeout"}
+            socket.close()
+            current_app.logger.debug("Worker connection timeout")
 
-    response["version"] = __version__
-    response["latency"] = time.perf_counter() - start_time
+        response["version"] = __version__
+        response["latency"] = time.perf_counter() - start_time
 
-    return Response(json.dumps(response, default=str), mimetype="application/json")
+        return Response(json.dumps(response, default=str), mimetype="application/json")
