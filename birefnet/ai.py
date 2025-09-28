0ddca07dd9a4cfcf4c7c4bb934e33ec70bc2902f
Bogdan Dobrica <bdobrica@gmail.com>
2025-09-28 17:45:23 +0300

    Update ai.py

--- birefnet/ai.py
+++ birefnet/ai.py
@@ -60,6 +60,7 @@ class AIDaemon(Daemon):
             # Image processing
             transform_image = transforms.Compose(
                 [
+                    transforms.ToTensor(),
                     transforms.Resize((1024, 1024)),
                     transforms.ToTensor(),
                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
